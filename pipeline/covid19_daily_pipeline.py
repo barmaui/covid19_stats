1986fcde04be3dab8b4b2f3c82eca51d16491e49
maui <barmaui16@gmail.com>
2020-05-10 14:21:21 +0100

    date update

--- pipeline/covid19_daily_pipeline.py
+++ pipeline/covid19_daily_pipeline.py
@@ -12,7 +12,8 @@ class UserOptions(PipelineOptions):
 
 options = PipelineOptions()
 user_options = options.view_as(UserOptions)
-logging.info(options)
+logging.warning(options)
+logging.warning(user_options)
 google_cloud_options = options.view_as(GoogleCloudOptions)
 google_cloud_options.project = "covid19stats-273220"
 google_cloud_options.job_name = "daily-update-pipeline"
@@ -77,5 +78,4 @@ def run():
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.INFO)
     run()
