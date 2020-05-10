7e114bb486b0ce4b327a8b25a98c352217dd930c
maui <barmaui16@gmail.com>
2020-05-10 12:48:37 +0100

    date update

--- trigger_function/main.py
+++ trigger_function/main.py
@@ -1,18 +1,22 @@
-from googleapiclient.discovery import build
-from oauth2client.client import GoogleCredentials
-def launch_pipeline(request):
+def launch_pipeline(data, context):
+    from googleapiclient.discovery import build
+    from oauth2client.client import GoogleCredentials
+    from datetime import datetime, timedelta
     credentials = GoogleCredentials.get_application_default()
+    yesterday = datetime.today() - timedelta(days=1)
+    date_y = yesterday.strftime('%Y-%m-%d')
     service = build('dataflow', 'v1b3', credentials=credentials)
     # Update the below four variables to reflect your requirements
     JOBNAME = "Daily Update COVID19 data"
     PROJECT = "covid19stats-273220"
     BUCKET = "covid19_pipeline_code"
     TEMPLATE = "daily_update_template"
-    GCSPATH = "gs://{bucket}/templates/{template}".format(bucket=BUCKET, template=TEMPLATE)
+    GCSPATH = "gs://{bucket}/template/{template}".format(bucket=BUCKET, template=TEMPLATE)
     BODY = {
         "jobName": "{jobname}".format(jobname=JOBNAME),
         "parameters": {
-            "region": "us-central1"
+            "region": "us-central1",
+            "input_date": f'{date_y}'
         },
         "environment": {
             "tempLocation": "gs://{bucket}/temp".format(bucket=BUCKET),
